c266dcb31d75df0aac69e57850be6f61b9d37dc9
AndiPratama <30918903+andipratama05@users.noreply.github.com>
2020-11-02 19:43:05 +0700

    Add files via upload

---
+++ precache-manifest.1ccbe9f1eb768774ce04d3abc0d3226e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2be481016b3387d561d928a5a62b22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24b9dc44c43c366db244",
+    "url": "/static/css/2.e6157a56.chunk.css"
+  },
+  {
+    "revision": "ff66c643bff061e23fd8",
+    "url": "/static/css/main.523833f0.chunk.css"
+  },
+  {
+    "revision": "24b9dc44c43c366db244",
+    "url": "/static/js/2.5068b164.chunk.js"
+  },
+  {
+    "revision": "3a0685a4e6f8157163147f00af59c181",
+    "url": "/static/js/2.5068b164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff66c643bff061e23fd8",
+    "url": "/static/js/main.dd6160dc.chunk.js"
+  },
+  {
+    "revision": "78ec66d2ceed516e159f",
+    "url": "/static/js/runtime-main.483b57af.js"
+  }
+]);
